510f3b94fef99bcf6756277a265f5ff3d94f7ece
CheaytoHussein <cheayto27@gmail.com>
2023-09-25 13:27:39 +0300

    creating noise and jitter functions

--- pc_worker/celery_worker.py
+++ pc_worker/celery_worker.py
@@ -1,3 +1,6 @@
+from random import random
+
+import numpy as np
 import open3d as o3d
 import json
 from celery import Celery
@@ -19,10 +22,39 @@ def voxel_downsampling(pointcloud_path: str, voxel_size: float):
 
 
 @celery_worker.task(name="noise", queue="noise_queue")
-def add_noise():
-    pass
+def add_noise(ply_path: str, noise_range: int):
+    pcd = o3d.io.read_point_cloud(ply_path)
+
+    pcd_points = np.asarray(pcd.points)
+    pcd_points_length, _ = pcd_points.shape
+    for point, index in zip(pcd_points, range(pcd_points_length)):
+        pcd_points[index] = [
+            coordinate + (((-1) ** round(random())) * round(random() * noise_range))
+            for coordinate in pcd_points[index]
+        ]
+
+    pcd.points = o3d.utility.Vector3dVector(pcd_points)
+    o3d.io.write_point_cloud(ply_path, pcd)
+
+    # to visualize the model
+    # pcd.paint_uniform_color([0.5, 0.5, 0.5])
+    # o3d.visualization.draw_geometries([pcd])
 
 
 @celery_worker.task(name="jittering", queue="jittering_queue")
-def jitter():
-    pass
+def jitter(ply_path: str, jitter_percentage):
+    pcd = o3d.io.read_point_cloud(ply_path)
+    pcd_points = np.asarray(pcd.points)
+    pcd_points_length, _ = pcd_points.shape
+
+    jitter_number = int((jitter_percentage * pcd_points_length) / 100)
+    random_seed = int(random() * (pcd_points_length - jitter_number))
+    print(len(pcd_points[random_seed:(random_seed + jitter_number)]))
+    np.random.shuffle(pcd_points[random_seed:(random_seed + jitter_number)])
+    print(len(pcd_points[random_seed:(random_seed + jitter_number)]))
+
+    pcd.points = o3d.utility.Vector3dVector(pcd_points)
+    o3d.io.write_point_cloud(ply_path, pcd)
+
+
+
